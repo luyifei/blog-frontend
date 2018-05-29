8340c08fd8f15ebcbb52d40f9a32df0e9528e9ec
sunny <sunny899030@gmail.com>
2018-05-29 09:01:43 +0800

    详情页面

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,7 @@ var Main = {
             + "<a href=\"" + item.id + "\" >" + item.title + "</a></div>"
             + "<div class=\"summary\">" + item.summary + "</div>"
             + "</div><div class=\"list_userbar\"><span class=\"text\">" + item.createTimeStr
-            + "</span><a class=\"floatR\" href=\"" + item.id + "\"><span class=\"num\">"
+            + "</span><a class=\"floatR\" href=\"detail.html?id=" + item.id + "\"><span class=\"num\">"
             + item.readCount + "</span>&nbsp;<span class=\"text\">阅读</span></a></div></div></li>";
         return content;
     },
